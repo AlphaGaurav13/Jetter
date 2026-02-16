5daefc15c78dc43c43a3cece9c840bbf10d280e0
Gaurav132005 <gauravkkr345@gmail.com>
2026-02-16 19:56:50 +0530

    Fixed minor Backend Bugs in routes

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,43 +1,43 @@
 import express from "express";
-import { Z } from 'zod';
-import jwt from 'jsonwebtoken';
+import { z } from "zod";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
 const router = express.Router();
-import User from '../models/User.js';
 
-
-// zod schema 
-const signupSchema = Z.object({
-    email: Z.string().email(),
-    password: Z.string().min(6)
+const signupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6)
 });
 
 router.post("/signup", async (req, res) => {
-    try{
+    try {
         const parsed = signupSchema.safeParse(req.body);
-        if(!parsed.success) {
-            return res.status(400).json({message: "Invalid inputs"});
+
+        if (!parsed.success) {
+            return res.status(400).json({ message: "Invalid inputs" });
         }
 
         const { email, password } = parsed.data;
+
         const existingUser = await User.findOne({ email });
 
-        if(existingUser) {
+        if (existingUser) {
             return res.status(409).json({ message: "User Already Exists" });
         }
 
         const user = await User.create({ email, password });
 
-        const token  = jwt.sign(
+        const token = jwt.sign(
             { userId: user._id },
             process.env.JWT_SECRET
         );
 
         res.json({ token });
 
-    }catch(err) {
+    } catch (err) {
         res.status(500).json({ message: "Server Error" });
     }
-})
-
+});
 
 export default router;
